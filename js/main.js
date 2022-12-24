8ce3826b40b386288a382f20f2f4954fb4d65b74
Clara <clarapachecodev@gmail.com>
2022-12-24 13:10:31 -0300

    Fix logic for upper bar menu disappear  when nav_menu shows

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const navMenu = document.querySelector('.nav__menu')
 const navToggle = document.querySelector('.nav__toggle')
 const navClose = document.querySelector('.nav__close')
+const navUpperBarShow = document.querySelector('.upper-bar-open')
 
 console.log(navMenu)
 console.log(navToggle)
 console.log(navClose)
+console.log(navUpperBarShow)
 
 
 //================= MENU SHOW ======================
@@ -13,6 +15,7 @@ console.log(navClose)
 if(navToggle){
   navToggle.addEventListener('click',() =>{
     navMenu.classList.add('show-menu')
+    navUpperBarShow.style.display='none'
   })
 }
 
@@ -22,6 +25,6 @@ if(navToggle){
 if(navClose){
   navClose.addEventListener('click',() =>{
     navMenu.classList.remove('show-menu')
-
-  })
-}
+    navUpperBarShow.style.display='flex'
+  }
+  )}
